2e5a8bd9a68757b796cf372d44613c2a0451f463
parthenocissus <uros.krcadinac@gmail.com>
2019-11-16 19:37:24 +0100

    added host without debug

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hedgehog Street'
-
-
-@app.route('/test/')
-def hello_world_test():
+@app.route('/flasktest/')
+def flasktest():
     return 'Hedgehog Street 2'
 
 
